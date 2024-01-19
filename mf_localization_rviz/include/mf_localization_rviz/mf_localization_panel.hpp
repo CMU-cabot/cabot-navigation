4c3732693d6028e505064c71c688816a470cf1a2
Daisuke Sato <daisukes@cmu.edu>
2024-01-19 14:58:33 -0500

    add status to mf_localization_rviz view
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- mf_localization_rviz/include/mf_localization_rviz/mf_localization_panel.hpp
+++ mf_localization_rviz/include/mf_localization_rviz/mf_localization_panel.hpp
@@ -22,10 +22,16 @@
 #define MF_LOCALIZATION_RVIZ__MF_LOCALIZATION_PANEL_HPP_
 
 #include <QPushButton>
+#include <QLabel>
 
 #include <rviz_common/panel.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <cabot_msgs/msg/pose_log.hpp>
+#include <gazebo_msgs/msg/model_states.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include <std_msgs/msg/int64.hpp>
+#include "mf_localization_msgs/msg/mf_localize_status.hpp"
 #include "mf_localization_msgs/srv/restart_localization.hpp"
 #include "mf_localization_msgs/srv/floor_change.hpp"
 
@@ -42,6 +48,13 @@ public:
   void onInitialize() override;
   void load(const rviz_common::Config & config);
   void save(rviz_common::Config config) const;
+  void pose_callback(const cabot_msgs::msg::PoseLog::SharedPtr msg);
+  void model_callback(const gazebo_msgs::msg::ModelStates::SharedPtr msg);
+  void floor_callback(const std_msgs::msg::Int64::SharedPtr msg);
+  void floor_raw_callback(const std_msgs::msg::Float64::SharedPtr msg);
+  void area_callback(const std_msgs::msg::Int64::SharedPtr msg);
+  void status_callback(const mf_localization_msgs::msg::MFLocalizeStatus::SharedPtr msg);
+  void updateMessage();
 
 private Q_SLOTS:
   void updateTopic();
@@ -57,11 +70,25 @@ private:
   QPushButton * up_button_;
   QPushButton * down_button_;
   QPushButton * memo_button_;
+  QLabel * statusLabel_;
 
   rclcpp::Node::SharedPtr node_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::Subscription<cabot_msgs::msg::PoseLog>::SharedPtr pose_sub_;
+  rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr model_sub_;
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr floor_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr floor_raw_sub_;
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr area_sub_;
+  rclcpp::Subscription<mf_localization_msgs::msg::MFLocalizeStatus>::SharedPtr status_sub_;
   rclcpp::Client<mf_localization_msgs::srv::RestartLocalization>::SharedPtr restart_localization_client_;
   rclcpp::Client<mf_localization_msgs::srv::FloorChange>::SharedPtr floor_change_client_;
+
+  cabot_msgs::msg::PoseLog::SharedPtr pose_msg_;
+  gazebo_msgs::msg::ModelStates::SharedPtr model_msg_;
+  std_msgs::msg::Int64::SharedPtr floor_msg_;
+  std_msgs::msg::Float64::SharedPtr floor_raw_msg_;
+  std_msgs::msg::Int64::SharedPtr area_msg_;
+  mf_localization_msgs::msg::MFLocalizeStatus::SharedPtr status_msg_;
 };
 }  // namespace rviz_common
 
--- mf_localization_rviz/src/mf_localization_panel.cpp
+++ mf_localization_rviz/src/mf_localization_panel.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <QFont>
 #include <QLabel>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
@@ -25,6 +26,9 @@
 #include <memory>
 #include "mf_localization_rviz/mf_localization_panel.hpp"
 #include <rviz_common/display_context.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace rviz_common
 {
@@ -41,20 +45,26 @@ MultifloorLocalizationPanel::MultifloorLocalizationPanel(QWidget * parent)
   auto label = new QLabel("Floor:");
   label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 
+  statusLabel_ = new QLabel("");
+  statusLabel_->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+  QFont font("Monospace");
+  font.setStyleHint(QFont::TypeWriter);
+  statusLabel_->setFont(font);
+  updateMessage();
+
   // layout
   QVBoxLayout * vlayout = new QVBoxLayout;
   QHBoxLayout * hlayout1 = new QHBoxLayout;
   QHBoxLayout * hlayout2 = new QHBoxLayout;
   QHBoxLayout * hlayout3 = new QHBoxLayout;
+  QHBoxLayout * hlayout4 = new QHBoxLayout;
 
   hlayout1->addWidget(restart_button_);
-
   hlayout2->addWidget(label);
   hlayout2->addWidget(up_button_);
   hlayout2->addWidget(down_button_);
-
-  hlayout3->addWidget(memo_button_);
-
+  hlayout3->addWidget(statusLabel_);
+  hlayout4->addWidget(memo_button_);
 
   auto lineA = new QFrame;
   lineA->setFrameShape(QFrame::HLine);
@@ -64,10 +74,11 @@ MultifloorLocalizationPanel::MultifloorLocalizationPanel(QWidget * parent)
   lineB->setFrameShadow(QFrame::Sunken);
 
   vlayout->addLayout(hlayout1);
-  vlayout->addWidget(lineA);
   vlayout->addLayout(hlayout2);
-  vlayout->addWidget(lineB);
+  vlayout->addWidget(lineA);
   vlayout->addLayout(hlayout3);
+  vlayout->addWidget(lineB);
+  vlayout->addLayout(hlayout4);
 
   setLayout(vlayout);
 
@@ -91,6 +102,137 @@ void MultifloorLocalizationPanel::onInitialize()
 void MultifloorLocalizationPanel::updateTopic()
 {
   pub_ = node_->create_publisher<std_msgs::msg::String>("memo", 1);
+  pose_sub_ = node_->create_subscription<cabot_msgs::msg::PoseLog>("/cabot/pose_log", 10,
+    std::bind(&MultifloorLocalizationPanel::pose_callback, this, std::placeholders::_1));
+  model_sub_ = node_->create_subscription<gazebo_msgs::msg::ModelStates>("/gazebo/model_states", 10,
+    std::bind(&MultifloorLocalizationPanel::model_callback, this, std::placeholders::_1));
+  floor_sub_ = node_->create_subscription<std_msgs::msg::Int64>("current_floor", 10,
+    std::bind(&MultifloorLocalizationPanel::floor_callback, this, std::placeholders::_1));
+  floor_raw_sub_ = node_->create_subscription<std_msgs::msg::Float64>("current_floor_raw", 10,
+    std::bind(&MultifloorLocalizationPanel::floor_raw_callback, this, std::placeholders::_1));
+  area_sub_ = node_->create_subscription<std_msgs::msg::Int64>("current_area", 10,
+    std::bind(&MultifloorLocalizationPanel::area_callback, this, std::placeholders::_1));
+  status_sub_ = node_->create_subscription<mf_localization_msgs::msg::MFLocalizeStatus>("localize_status", 10,
+    std::bind(&MultifloorLocalizationPanel::status_callback, this, std::placeholders::_1));
+}
+
+void MultifloorLocalizationPanel::pose_callback(const cabot_msgs::msg::PoseLog::SharedPtr msg) {
+  pose_msg_ = msg;
+  updateMessage();
+}
+
+void MultifloorLocalizationPanel::model_callback(const gazebo_msgs::msg::ModelStates::SharedPtr msg) {
+  model_msg_ = msg;
+  updateMessage();
+}
+
+void MultifloorLocalizationPanel::floor_callback(const std_msgs::msg::Int64::SharedPtr msg) {
+  floor_msg_ = msg;
+  updateMessage();
+}
+
+void MultifloorLocalizationPanel::floor_raw_callback(const std_msgs::msg::Float64::SharedPtr msg) {
+  floor_raw_msg_ = msg;
+  updateMessage();
+}
+
+void MultifloorLocalizationPanel::area_callback(const std_msgs::msg::Int64::SharedPtr msg) {
+  area_msg_ = msg;
+  updateMessage();
+}
+
+void MultifloorLocalizationPanel::status_callback(const mf_localization_msgs::msg::MFLocalizeStatus::SharedPtr msg) {
+  status_msg_ = msg;
+  updateMessage();
+}
+
+// https://stackoverflow.com/a/26221725
+template<typename ... Args>
+std::string string_format(const std::string & format, Args ... args)
+{
+  int size_s = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1;  // Extra space for '\0'
+  if (size_s <= 0) {throw std::runtime_error("Error during formatting.");}
+  size_t size = static_cast<size_t>( size_s );
+  std::unique_ptr<char[]> buf(new char[size]);
+  std::snprintf(buf.get(), size, format.c_str(), args ...);
+  return std::string(buf.get(), buf.get() + size - 1);  // We don't want the '\0' inside
+}
+// end
+
+void MultifloorLocalizationPanel::updateMessage() {
+  std::string floor = " ";
+  std::string floor_str = "   ";
+  if (floor_msg_) {
+    floor = string_format("%+2d", floor_msg_->data);
+    if (floor_msg_->data < 0) {
+      floor_str = string_format("B%dF", -floor_msg_->data);
+    } else {
+      floor_str = string_format("%2dF", floor_msg_->data+1);
+    }
+  }
+  std::string floor_raw = "     ";
+  if (floor_raw_msg_) {
+    floor_raw = string_format("%+5.2f", floor_raw_msg_->data);
+  }
+  std::string area = " ";
+  if (area_msg_) {
+    area = string_format("%d", area_msg_->data);
+  }
+  std::string status = "";
+  if (status_msg_) {
+    if (status_msg_->status == 0) {
+      status = "UNKNOWN";
+    } else if (status_msg_->status == 1) {
+      status = "LOCATING";
+    } else if (status_msg_->status == 2) {
+      status = "TRACKING";
+    } else if (status_msg_->status == 3) {
+      status = "UNRELIABLE";
+    }
+  }
+  std::string x = "         ";
+  std::string y = "         ";
+  std::string a = "    ";
+  std::string lat = "         ";
+  std::string lng = "         ";
+  if (pose_msg_) {
+    x = string_format("%+7.2f   ", pose_msg_->pose.position.x);
+    y = string_format("%+7.2f   ", pose_msg_->pose.position.y);
+    auto q = pose_msg_->pose.orientation;
+    tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+    tf2::Matrix3x3 m(quat);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    a = string_format("%+4.0f", yaw/M_PI*180);
+    lat = string_format("%+10.5f", pose_msg_->lat);
+    lng = string_format("%+10.5f", pose_msg_->lng);
+  }
+  std::string model = "";
+  if (model_msg_) {
+    unsigned long i;
+    for(i = 0; i < model_msg_->name.size(); i++) {
+      if (model_msg_->name[i] == "mobile_base") {
+        auto gx = string_format("%+7.2f   ", model_msg_->pose[i].position.x);
+        auto gy = string_format("%+7.2f   ", model_msg_->pose[i].position.y);
+        auto gz = string_format("%+7.2f   ", model_msg_->pose[i].position.z);
+        model = string_format("\nGazebo : (%s, %s, %s)", gx.c_str(), gy.c_str(), gz.c_str());
+        break;
+      }
+    }
+  }
+  QString qString = QString::fromStdString(
+      string_format("Status : %s\n"
+                    "Floor  : %s (f=%s, fr=%s, a=%s)\n"
+                    "Pose   : (%s, %s) @ %s deg\n"
+                    "LatLng : (%s, %s)"
+                    "%s",
+                    status.c_str(),
+                    floor_str.c_str(), floor.c_str(), floor_raw.c_str(), area.c_str(),
+                    x.c_str(), y.c_str(), a.c_str(),
+                    lat.c_str(), lng.c_str(),
+                    model.c_str()
+                    ));
+  statusLabel_->setText(qString);
 }
 
 void MultifloorLocalizationPanel::updateService()
