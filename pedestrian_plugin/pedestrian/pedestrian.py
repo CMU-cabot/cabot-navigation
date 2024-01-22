6556fa0f55ac5ab7940b264fb0be2ad25ad59158
Daisuke Sato <daisukes@cmu.edu>
2024-01-22 16:40:15 -0500

    update pedestrian.py
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- pedestrian_plugin/pedestrian/pedestrian.py
+++ pedestrian_plugin/pedestrian/pedestrian.py
@@ -1,6 +1,10 @@
 import sys
 import ros
 
+count = 0
 def onUpdate(*args):
-    ros.info(str(args))
-    return None
+    global count
+    count +=1
+    r = (0.001*count, 0.0, 0.0, 0.0, 0.0, 0.0, 0.001*count)
+    ros.info(f"{str(args)}")
+    return r
