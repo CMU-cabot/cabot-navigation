6fabf7f0859524f017003c1499dba3d01593e2d9
fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2024-07-02 13:09:31 +0900

    put image date into a separate bag file
    Signed-off-by: fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- script/record_bag.sh
+++ script/record_bag.sh
@@ -6,6 +6,7 @@ scriptdir=`dirname $0`
 cd $scriptdir
 scriptdir=`pwd`
 pid=
+image_pid=
 
 # load utility functions
 source $scriptdir/cabot_util.sh
@@ -15,21 +16,32 @@ trap signal INT TERM
 function signal() {
     echo "trap record_bag.sh"
     kill -2 $pid
+    if [[ $record_cam -eq 1 ]]; then
+        kill -2 $image_pid
+    fi
 
-    while kill -0 $pid 2> /dev/null; do
-	if [[ $count -eq 15 ]]; then
-	    blue "escalate to SIGTERM $pid"
-	    com="kill -TERM $pid"
-	    eval $com
-	fi
-	if [[ $count -eq 30 ]]; then
-	    blue "escalate to SIGKILL $pid"
-	    com="kill -KILL $pid"
-	    eval $com
-	fi
+    while kill -0 $pid 2> /dev/null || ([[ $record_cam -eq 1 ]] && kill -0 $image_pid 2> /dev/null); do
+        if [[ $count -eq 15 ]]; then
+            blue "escalate to SIGTERM $pid"
+            com="kill -TERM $pid"
+            eval $com
+            if [[ $record_cam -eq 1 ]]; then
+                com="kill -TERM $image_pid"
+                eval $com
+            fi
+        fi
+        if [[ $count -eq 30 ]]; then
+            blue "escalate to SIGKILL $pid"
+            com="kill -KILL $pid"
+            eval $com
+            if [[ $record_cam -eq 1 ]]; then
+                com="kill -KILL $image_pid"
+                eval $com
+            fi
+        fi
         echo "waiting $0 $pid"
         snore 1
-	count=$((count+1))
+        count=$((count+1))
     done
 
     exit
@@ -55,9 +67,10 @@ record_cam=${CABOT_ROSBAG_RECORD_CAMERA:=0}
 
 exclude_topics_file="rosbag2-exclude-topics.txt"
 exclude_camera_topics="/.*/image_raw.*"
-if [[ $record_cam -eq 1 ]]; then
-    exclude_camera_topics='/.*/image_raw$'  # record compressed images
-fi
+# if [[ $record_cam -eq 1 ]]; then
+#     # exclude_camera_topics='/.*/image_raw$'  # record compressed images
+#     com="ros2 bag record ${use_sim_time} -s ${backend} ${compression} -p ${interval} -e '/.*camera_info|/.*image_raw' -x '/.*image_raw$' ${hidden_topics} ${qos_option} -o $ROS_LOG_DIR/image_topics 2>&1 &"
+# fi
 lines=$(cat $scriptdir/${exclude_topics_file})
 exclude_topics=$(echo -n "${lines}" | tr '\n' '|')
 exclude_topics="${exclude_topics}|${exclude_camera_topics}"
@@ -81,6 +94,14 @@ fi
 echo $com
 eval $com
 pid=$!
+
+if [[ $record_cam -eq 1 ]]; then
+    # exclude_camera_topics='/.*/image_raw$'  # record compressed images
+    image_com="ros2 bag record ${use_sim_time} -s ${backend} ${compression} -p ${interval} -e '/.*camera_info|/.*image_raw' -x '/.*image_raw$' ${hidden_topics} ${qos_option} -o $ROS_LOG_DIR/image_topics 2>&1 &"
+    echo $image_com
+    eval $image_com
+    image_pid=$!
+fi
 
 ## wait until it is terminated by the user
 while [ 1 -eq 1 ];
