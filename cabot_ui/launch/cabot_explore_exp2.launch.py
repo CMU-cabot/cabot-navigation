f2514a180c68cf6fdb9f8d01f76b3d47923212e2
Kohei Uehara <kouhei.uehara@jst.go.jp>
2024-08-18 18:42:55 +0900

    [skip ci] [update] implement cabot_explore_exp2.launch.py
    Signed-off-by: Kohei Uehara <kouhei.uehara@jst.go.jp>

--- cabot_ui/launch/cabot_explore_exp2.launch.py
+++ cabot_ui/launch/cabot_explore_exp2.launch.py
@@ -6,36 +6,49 @@ def generate_launch_description():
     log_dir = LaunchConfiguration('log_dir')
     apikey = LaunchConfiguration('apikey')
 
-    return LaunchDescription([        
-        # surronding explain mode
+    return LaunchDescription([    
+        # main code
         Node(
-            package='cabot_ui',
-            executable='exploration_image_explain.py',
-            name='exploration_image_explain_surronding',
-            output='screen',
+            package="cabot_ui",
+            executable="exploration_main_loop.py",
+            name="exploration_main_loop",
+            output="screen",
             parameters=[
-                {'mode': 'surronding_explain_mode'},
-                {'should_speak': True},
-                {'log_dir': log_dir},                
-                {'debug': False},
-                {'once': False},
-                {'no_explain_mode': False},
-                {'is_sim': True},
-                {'apikey': apikey}
-                
-            ]
+                {"log_dir": log_dir},
+                {"dist_filter": True},
+                {"is_sim": True},
+            ],
         ),
 
-        # chat server
-        Node(
-            package='cabot_ui',
-            executable='exploration_chat_server.py',
-            name='exploration_chat_server',
-            output='screen',
-            parameters=[
-                {'log_dir': log_dir},
-                {'use_openai': True},  # Set this to False if you don't want to use OpenAI
-                {'apikey': apikey}
-            ],
-        )
+        # surronding explain mode
+        # Node(
+        #     package='cabot_ui',
+        #     executable='exploration_image_explain.py',
+        #     name='exploration_image_explain_surronding',
+        #     output='screen',
+        #     parameters=[
+        #         {'mode': 'surronding_explain_mode'},
+        #         {'should_speak': True},
+        #         {'log_dir': log_dir},                
+        #         {'debug': False},
+        #         {'once': False},
+        #         {'no_explain_mode': False},
+        #         {'is_sim': True},
+        #         {'apikey': apikey}
+                
+        #     ]
+        # ),
+
+        # # chat server
+        # Node(
+        #     package='cabot_ui',
+        #     executable='exploration_chat_server.py',
+        #     name='exploration_chat_server',
+        #     output='screen',
+        #     parameters=[
+        #         {'log_dir': log_dir},
+        #         {'use_openai': True},  # Set this to False if you don't want to use OpenAI
+        #         {'apikey': apikey}
+        #     ],
+        # )
     ])
