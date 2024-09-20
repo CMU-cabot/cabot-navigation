49d7158e3900d447cd2fe962a642009a652e55b1
YumaMatsumura-kufusha <y.matsumura@kufusha.com>
2024-09-20 10:53:46 +0900

    update copyright
    Signed-off-by: YumaMatsumura-kufusha <y.matsumura@kufusha.com>

--- cabot_navigation2/test/evaluator.py
+++ cabot_navigation2/test/evaluator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ###############################################################################
-# Copyright (c) 2024  IBM Corporation
+# Copyright (c) 2024  IBM Corporation and Miraikan
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
