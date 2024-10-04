f65fe8659f47525deecbaa648d3180b59a91b11f
Daisuke Sato <daisukes@cmu.edu>
2024-10-04 17:20:46 +0900

    bugfix
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_ui/cabot_ui/navigation.py
+++ cabot_ui/cabot_ui/navigation.py
@@ -145,6 +145,7 @@ class BufferProxy():
         self.countPub = node.create_publisher(std_msgs.msg.Int32, "transform_count", 10, callback_group=MutuallyExclusiveCallbackGroup())
         self.transformMap = {}
         self.min_interval = rclpy.duration.Duration(seconds=0.2)
+        self.lock = threading.Lock()
 
     def debug(self):
         if not hasattr(self, "count"):
@@ -156,55 +157,64 @@ class BufferProxy():
 
     # buffer interface
     def lookup_transform(self, target, source, time=None):
-        # find the latest saved transform first
-        key = f"{target}-{source}"
-        now = self._clock.now()
-        if key in self.transformMap:
-            (transform, last_time) = self.transformMap[key]
-            if now - last_time < self.min_interval:
-                self._logger.debug(f"found old lookup_transform({target}, {source}, {(now - last_time).nanoseconds/1000000000:.2f}sec)")
-                return transform
+        with self.lock:
+           # find the latest saved transform first
+           key = f"{target}-{source}"
+           now = self._clock.now()
+           if key in self.transformMap:
+               (transform, last_time) = self.transformMap[key]
+               if now - last_time < self.min_interval:
+                   self._logger.debug(f"found old lookup_transform({target}, {source}, {(now - last_time).nanoseconds/1000000000:.2f}sec)")
+                   return transform
 
-        if __debug__:
-            self.debug()
-        self._logger.debug(f"lookup_transform({target}, {source})")
-        req = LookupTransform.Request()
-        req.target_frame = target
-        req.source_frame = source
-        if not self.lookup_transform_service.wait_for_service(timeout_sec=1.0):
-            raise RuntimeError("lookup transform service is not available")
-        # usage of call() can cause SIGSEGV at exit due to infinite wait
-        # however, call(req, timeout_sec=1.0) is implemented in J-turtle release not in humble
-        #
-        # result = self.lookup_transform_service.call(req)
-        #
-        # so implemented call with timeout from here
-        # reference implementation is
-        # https://github.com/ros2/rclpy/blob/7a7f23e0d7e51dd244001ef606b97f1153e5a97e/rclpy/rclpy/client.py#L72-L110
-        event = threading.Event()
+           if __debug__:
+               self.debug()
+           self._logger.debug(f"lookup_transform({target}, {source})")
+           req = LookupTransform.Request()
+           req.target_frame = target
+           req.source_frame = source
+           if not self.lookup_transform_service.wait_for_service(timeout_sec=1.0):
+               self._logger.debug(f"lookup_transform timeout")
+               raise RuntimeError("lookup transform service is not available")
+           # usage of call() can cause SIGSEGV at exit due to infinite wait
+           # however, call(req, timeout_sec=1.0) is implemented in J-turtle release not in humble
+           #
+           # result = self.lookup_transform_service.call(req)
+           #
+           # so implemented call with timeout from here
+           # reference implementation is
+           # https://github.com/ros2/rclpy/blob/7a7f23e0d7e51dd244001ef606b97f1153e5a97e/rclpy/rclpy/client.py#L72-L110
+           event = threading.Event()
 
-        def unblock(future):
-            nonlocal event
-            event.set()
-        future = self.lookup_transform_service.call_async(req)
-        future.add_done_callback(unblock)
-        # Check future.done() before waiting on the event.
-        # The callback might have been added after the future is completed,
-        # resulting in the event never being set.
-        if not future.done():
-            if not event.wait(1.0):
-                # Timed out. remove_pending_request() to free resources
-                self.remove_pending_request(future)
-                raise RuntimeError("timeout")
-        if future.exception() is not None:
-            raise future.exception()
-        result = future.result()
-        # sync call end here
+           def unblock(future):
+               try:
+                   nonlocal event
+                   event.set()
+               except:
+                   self._logger.error(traceback.format_exc())
+           future = self.lookup_transform_service.call_async(req)
+           future.add_done_callback(unblock)
+           # Check future.done() before waiting on the event.
+           # The callback might have been added after the future is completed,
+           # resulting in the event never being set.
+           self._logger.debug("waiting lookup_transform service")
+           if not future.done():
+               if not event.wait(1.0):
+                   self._logger.debug("lookup_transform service timeout")
+                   # Timed out. remove_pending_request() to free resources
+                   self.remove_pending_request(future)
+                   raise RuntimeError("timeout")
+           if future.exception() is not None:
+               raise future.exception()
+           result = future.result()
+           # sync call end here
 
-        if result.error.error > 0:
-            raise RuntimeError(result.error.error_string)
-        self.transformMap[key] = (result.transform, now)
-        return result.transform
+           if result.error.error > 0:
+               self._logger.debug("got error")
+               raise RuntimeError(result.error.error_string)
+           self.transformMap[key] = (result.transform, now)
+           self._logger.debug(f"got transform {result}")
+           return result.transform
 
     def transform(self, pose_stamped, target):
         do_transform = tf2_ros.TransformRegistration().get(type(pose_stamped))
@@ -747,7 +757,7 @@ class Navigation(ControlBase, navgoal.GoalInterface):
         self._logger.info(F"navigation.{util.callee_name()} called")
         if self.lock.acquire():
             if self._loop_handle is None:
-                self._loop_handle = self._node.create_timer(0.1, self._check_loop, callback_group=self._main_callback_group)
+                self._loop_handle = self._node.create_timer(0.1, self._check_loop, callback_group=MutuallyExclusiveCallbackGroup())
             self.lock.release()
 
     def _stop_loop(self):
