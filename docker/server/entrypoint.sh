292383f1cddcc0924155468594c55c6658799242
t-hatakeyam <t.hatakeyama@kufusha.com>
2024-10-15 17:32:40 +0900

    server UID/GID switching
    Signed-off-by: t-hatakeyam <t.hatakeyama@kufusha.com>

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-set -e
 
 S_UID=${S_UID:-1001}
 S_GID=${S_GID:-1001}
 
 CONT_UID=$(id -u runner_user)
 CONT_GID=$(id -g runner_user)
+
 if [[ $CONT_UID -ne $S_UID ]] || [[ $CONT_GID -ne $S_GID ]]; then
+    echo "Updating $USERNAME with UID:$S_UID and GID:$S_GID"
     # Update user and group ID to match S
-    usermod -u $S_UID runner_user
     groupmod -g $S_GID runner_user
+    usermod -u $S_UID -g $S_GID runner_user
     chown -R "$S_UID:$S_GID" /home/runner_user
 fi
 
-WORKDIR=$(pwd)
-
-exec gosu runner_user bash -c "cd $WORKDIR && exec $*"
+exec gosu runner_user "$@"
