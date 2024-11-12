21f25e0486e36ad6cd04e078e4ef042ea78792cc
Yukiya Nakai <yukiya.nakai@alpsalpine.com>
2024-11-13 08:55:08 +0900

    add copyright
    Signed-off-by: Yukiya Nakai <yukiya.nakai@alpsalpine.com>

--- cabot_ui/cabot_ui/turn_detector.py
+++ cabot_ui/cabot_ui/turn_detector.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2020, 2022  Carnegie Mellon University
+# Copyright (c) 2024  ALPS ALPINE CO., LTD.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
