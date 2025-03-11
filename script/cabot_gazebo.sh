41d29de8986fcbbedb4d71d6f0b3559b4f57601b
yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>
2025-03-11 14:06:33 +0900

    update license
    Signed-off-by: yushi-minemura <67675657+yushi-minemura@users.noreply.github.com>

--- script/cabot_gazebo.sh
+++ script/cabot_gazebo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2023  Carnegie Mellon University
+# Copyright (c) 2023, 2025  Carnegie Mellon University and Miraikan
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
