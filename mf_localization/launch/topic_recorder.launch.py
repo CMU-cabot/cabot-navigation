25887afadcefb6141025126a8f392c2ad4754521
Masayuki Murata <muratams@jp.ibm.com>
2025-09-10 23:42:27 +0900

    add a node and a launch file to write topics to files
    Signed-off-by: Masayuki Murata <muratams@jp.ibm.com>

---
+++ mf_localization/launch/topic_recorder.launch.py
@@ -0,0 +1,156 @@
+#  Copyright (c) 2025  IBM Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from launch.logging import launch_config
+from launch.actions import SetEnvironmentVariable
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnShutdown
+from launch_ros.actions import SetParameter
+from launch_ros.descriptions import ParameterValue
+from cabot_common.launch import AppendLogDirPrefix
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    output_dir = LaunchConfiguration('output_dir')
+    points2 = LaunchConfiguration('points2')
+    compressed_image1 = LaunchConfiguration('compressed_image1')
+    compressed_image2 = LaunchConfiguration('compressed_image2')
+    compressed_image3 = LaunchConfiguration('compressed_image3')
+    compressed_image1_throttled = LaunchConfiguration('compressed_image1_throttled')
+    compressed_image2_throttled = LaunchConfiguration('compressed_image2_throttled')
+    compressed_image3_throttled = LaunchConfiguration('compressed_image3_throttled')
+    imu = LaunchConfiguration('imu')
+    wifi = LaunchConfiguration('wifi')
+    beacons = LaunchConfiguration('beacons')
+    fix = LaunchConfiguration('fix')
+    verbose = LaunchConfiguration('verbose')
+    # output = {'stderr': {'log'}}
+    output = "both"
+
+    return LaunchDescription([
+        # save all log file in the directory where the launch.log file is saved
+        SetEnvironmentVariable('ROS_LOG_DIR', launch_config.log_dir),
+        # append prefix name to the log directory for convenience
+        RegisterEventHandler(OnShutdown(on_shutdown=[AppendLogDirPrefix("mf_localization")])),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value="true",
+            description=ParameterValue(True)),
+
+        DeclareLaunchArgument(
+            'output_dir',
+            default_value=''
+            ),
+
+        DeclareLaunchArgument(
+            'points2',
+            default_value='/velodyne_points'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image1',
+            default_value='/rs1/color/image_raw/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image2',
+            default_value='/rs2/color/image_raw/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image3',
+            default_value='/rs3/color/image_raw/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image1_throttled',
+            default_value='/rs1/color/image_throttled/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image2_throttled',
+            default_value='/rs2/color/image_throttled/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'compressed_image3_throttled',
+            default_value='/rs3/color/image_throttled/compressed'
+            ),
+
+        DeclareLaunchArgument(
+            'imu',
+            default_value='/imu/data'
+            ),
+
+        DeclareLaunchArgument(
+            'wifi',
+            default_value='/esp32/wifi'
+            ),
+
+        DeclareLaunchArgument(
+            'beacons',
+            default_value='/wireless/beacons'
+            ),
+
+        DeclareLaunchArgument(
+            'fix',
+            default_value='/ublox/fix'
+            ),
+
+        DeclareLaunchArgument(
+            'verbose',
+            default_value='false',
+            description='True if output more'),
+
+        # set parameter for all nodes
+        SetParameter('use_sim_time', use_sim_time),
+
+        Node(
+            package='mf_localization',
+            executable='topic_recorder_node.py',
+            name='topic_recorder',
+            output=output,
+            parameters=[{
+                'output_dir': output_dir,
+                'verbose': verbose
+            }],
+            remappings=[
+                ('points2', points2),
+                ('compressed_image1', compressed_image1),
+                ('compressed_image2', compressed_image2),
+                ('compressed_image3', compressed_image3),
+                ('compressed_image1_throttled', compressed_image1_throttled),
+                ('compressed_image2_throttled', compressed_image2_throttled),
+                ('compressed_image3_throttled', compressed_image3_throttled),
+                ('imu', imu),
+                ('wifi', wifi),
+                ('beacons', beacons),
+                ('fix', fix),
+            ]
+        ),
+    ])
---
+++ mf_localization/script/topic_recorder_node.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2025  IBM Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+
+import signal
+import sys
+import yaml
+
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+
+from abc import abstractmethod
+from dataclasses import dataclass
+import importlib
+import json
+import math
+from pathlib import Path
+import traceback
+import types
+import queue
+import threading
+import time
+
+import cv2
+import numpy as np
+import open3d
+
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from rclpy.qos import qos_profile_sensor_data
+from rosidl_runtime_py import message_to_ordereddict
+
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseWithCovariance
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+from std_srvs.srv import SetBool
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import NavSatFix
+from sensor_msgs_py import point_cloud2
+
+from cabot_msgs.msg import PoseLog2
+
+
+class TopicRecorder:
+    def __init__(self, node: Node, output_dir: str):
+
+        self._node: Node = node
+        self._logger = node.get_logger()
+
+        self._output_dir = None
+        if output_dir != "":
+            self._output_dir = output_dir
+
+        # additional parameters
+        self._rotate_image1 = node.declare_parameter("rotate_image1", True).value
+        self._rotate_image2 = node.declare_parameter("rotate_image2", False).value
+        self._rotate_image3 = node.declare_parameter("rotate_image3", True).value
+
+        # callback group
+        self._cb_group = ReentrantCallbackGroup()
+
+        # data queue
+        self._point2_queue = queue.Queue(maxsize=1)
+        self._image1_queue = queue.Queue(maxsize=1)
+        self._image2_queue = queue.Queue(maxsize=1)
+        self._image3_queue = queue.Queue(maxsize=1)
+        self._image1_throttled_queue = queue.Queue(maxsize=1)
+        self._image2_throttled_queue = queue.Queue(maxsize=1)
+        self._image3_throttled_queue = queue.Queue(maxsize=1)
+        self._wifi_queue = queue.Queue(maxsize=1)
+        self._beacons_queue = queue.Queue(maxsize=1)
+        self._gnss_fix_queue = queue.Queue(maxsize=1)
+        self._pose_log2_queue = queue.Queue(maxsize=1)
+
+        # create subscribers
+        sensor_qos: QoSProfile = qos_profile_sensor_data
+        self.points2_sub = node.create_subscription(PointCloud2, "points2", self.points2_callback, qos_profile=sensor_qos, callback_group=self._cb_group)
+        self.image1_sub = node.create_subscription(CompressedImage, "compressed_image1", self.image1_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # front
+        self.image2_sub = node.create_subscription(CompressedImage, "compressed_image2", self.image2_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # right"
+        self.image3_sub = node.create_subscription(CompressedImage, "compressed_image3", self.image3_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # left
+        self.image1_throttled_sub = node.create_subscription(CompressedImage, "compressed_image1_throttled", self.image1_throttled_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # front
+        self.image2_throttled_sub = node.create_subscription(CompressedImage, "compressed_image2_throttled", self.image2_throttled_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # right"
+        self.image3_throttled_sub = node.create_subscription(CompressedImage, "compressed_image3_throttled", self.image3_throttled_callback, qos_profile=sensor_qos, callback_group=self._cb_group)  # left
+        self.wifi_sub = node.create_subscription(String, "wifi", self.wifi_callback, qos_profile=sensor_qos, callback_group=self._cb_group)
+        self.beacons_sub = node.create_subscription(String, "beacons", self.beacons_callback, qos_profile=sensor_qos, callback_group=self._cb_group)
+        self.gnss_fix_sub = node.create_subscription(NavSatFix, "fix", self.gnss_fix_callback, qos_profile=sensor_qos, callback_group=self._cb_group)
+
+        self.pose_log2_sub = node.create_subscription(PoseLog2, "/cabot/pose_log2", self.pose_log2_callback, qos_profile=sensor_qos, callback_group=self._cb_group)
+
+        # timer
+        self._node.create_timer(0.01, self.worker_tick, callback_group=self._cb_group)
+
+    def points2_callback(self, points2: PointCloud2):
+        # self._logger.info("points2_callback")
+        if self._point2_queue.full():
+            _ = self._point2_queue.get_nowait()
+        self._point2_queue.put_nowait(points2)
+
+    def image1_callback(self, image: CompressedImage):
+        if self._image1_queue.full():
+            _ = self._image1_queue.get_nowait()
+        self._image1_queue.put_nowait(image)
+
+    def image2_callback(self, image: CompressedImage):
+        if self._image2_queue.full():
+            _ = self._image2_queue.get_nowait()
+        self._image2_queue.put_nowait(image)
+
+    def image3_callback(self, image: CompressedImage):
+        if self._image3_queue.full():
+            _ = self._image3_queue.get_nowait()
+        self._image3_queue.put_nowait(image)
+
+    def image1_throttled_callback(self, image: CompressedImage):
+        if self._image1_throttled_queue.full():
+            _ = self._image1_throttled_queue.get_nowait()
+        self._image1_throttled_queue.put_nowait(image)
+
+    def image2_throttled_callback(self, image: CompressedImage):
+        if self._image2_throttled_queue.full():
+            _ = self._image2_throttled_queue.get_nowait()
+        self._image2_throttled_queue.put_nowait(image)
+
+    def image3_throttled_callback(self, image: CompressedImage):
+        if self._image3_throttled_queue.full():
+            _ = self._image3_throttled_queue.get_nowait()
+        self._image3_throttled_queue.put_nowait(image)
+
+    def wifi_callback(self, wifi: String):
+        if self._wifi_queue.full():
+            _ = self._wifi_queue.get_nowait()
+        self._wifi_queue.put_nowait(wifi)
+
+    def beacons_callback(self, beacons: String):
+        if self._beacons_queue.full():
+            _ = self._beacons_queue.get_nowait()
+        self._beacons_queue.put_nowait(beacons)
+
+    def gnss_fix_callback(self, fix: NavSatFix):
+        if self._gnss_fix_queue.full():
+            _ = self._gnss_fix_queue.get_nowait()
+        self._gnss_fix_queue.put_nowait(fix)
+
+    def pose_log2_callback(self, msg: PoseLog2):
+        if self._pose_log2_queue.full():
+            _ = self._pose_log2_queue.get_nowait()
+        self._pose_log2_queue.put_nowait(msg)
+
+    def worker_tick(self):
+        if self._point2_queue.qsize() > 0 \
+          and self._image1_queue.qsize() > 0 \
+          and self._wifi_queue.qsize() > 0\
+          and self._gnss_fix_queue.qsize() > 0:
+            pass
+        else:
+            return
+
+        now = self._node.get_clock().now()
+        timestamp = now.nanoseconds * 1e-9
+
+        try:
+            points2 = self._point2_queue.get_nowait()
+            image1 = self._image1_queue.get_nowait()
+            wifi = self._wifi_queue.get_nowait()
+            fix = self._gnss_fix_queue.get_nowait()
+            self._logger.info(F"points tick t = {timestamp}")
+
+            # points2 conversion
+            pts = []
+            for p in point_cloud2.read_points(points2, skip_nans=True, field_names=("x", "y", "z")):
+                pts.append(list(p))
+            pts = np.array(pts)
+            self._logger.info(F"pts = {pts}")
+
+            # save pointcloud
+            if self._output_dir is not None:
+                output_path = Path(self._output_dir)
+                # save pointcloud
+                pcd = open3d.geometry.PointCloud()
+                pcd.points = open3d.utility.Vector3dVector(pts)
+                pcd_path = output_path.joinpath(F"{timestamp}_points2.pcd")
+                open3d.io.write_point_cloud(pcd_path, pcd, compressed=True)
+
+                # save image1
+                img = cv2.imdecode(
+                    np.frombuffer(image1.data, dtype='uint8'), cv2.IMREAD_UNCHANGED
+                )
+                if self._rotate_image1:
+                    img = cv2.rotate(img, cv2.ROTATE_180)
+                img1_path = output_path.joinpath(F"{timestamp}_image1.jpg")
+                cv2.imwrite(str(img1_path), img)
+
+                if self._image2_queue.qsize() > 0:
+                    image2 = self._image2_queue.get_nowait()
+                    img = cv2.imdecode(
+                        np.frombuffer(image2.data, dtype='uint8'), cv2.IMREAD_UNCHANGED
+                    )
+                    if self._rotate_image2:
+                        img = cv2.rotate(img, cv2.ROTATE_180)
+                    img2_path = output_path.joinpath(F"{timestamp}_image2.jpg")
+                    cv2.imwrite(str(img2_path), img)
+
+                if self._image3_queue.qsize() > 0:
+                    image3 = self._image3_queue.get_nowait()
+                    img = cv2.imdecode(
+                        np.frombuffer(image3.data, dtype='uint8'), cv2.IMREAD_UNCHANGED
+                    )
+                    if self._rotate_image3:
+                        img = cv2.rotate(img, cv2.ROTATE_180)
+                    img3_path = output_path.joinpath(F"{timestamp}_image3.jpg")
+                    cv2.imwrite(str(img3_path), img)
+
+                # save wifi
+                wifi_json = json.loads(wifi.data)
+                wifi_path = output_path.joinpath(F"{timestamp}_wifi.json")
+                with open(wifi_path, "w") as f:
+                    json.dump(wifi_json, f, indent=2)
+
+                # beacons
+                if self._beacons_queue.qsize() > 0:
+                    beacons = self._beacons_queue.get_nowait()
+                    beacons_json = json.loads(beacons.data)
+                    beacons_path = output_path.joinpath(F"{timestamp}_beacons.json")
+                    with open(beacons_path, "w") as f:
+                        json.dump(beacons_json, f, indent=2)
+
+                # fix
+                fix_dict = message_to_ordereddict(fix)
+                fix_path = output_path.joinpath(F"{timestamp}_fix.json")
+                with open(fix_path, "w") as f:
+                    json.dump(fix_dict, f, indent=2)
+
+                # pose log2
+                if self._pose_log2_queue.qsize() > 0:
+                    pose_log2 = self._pose_log2_queue.get_nowait()
+                    pl2_dict = message_to_ordereddict(pose_log2)
+                    pl2_path = output_path.joinpath(F"{timestamp}_poselog2.json")
+                    with open(pl2_path, "w") as f:
+                        json.dump(pl2_dict, f, indent=2)
+
+        except queue.Empty:
+            self._logger.info(traceback.format_exc())
+            return
+
+
+def receiveSignal(signal_num, frame):
+    print("Received:", signal_num)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, receiveSignal)
+
+
+def main():
+    rclpy.init()
+    node = Node("topic_recorder")
+    logger = node.get_logger()
+
+    # parameter
+    output_dir = node.declare_parameter("output_dir", "").value
+    logger.info(F"output dir = {output_dir}")
+
+    topic_recorder: TopicRecorder = TopicRecorder(node, output_dir)
+
+    logger.info(F"created {type(topic_recorder).__name__}")
+
+    exec = MultiThreadedExecutor()
+    exec.add_node(node)
+    exec.spin()
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
